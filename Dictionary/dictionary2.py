7b27ebae7a76a9dba963ea8d8da57f6afff22f4f
Gaurav Jha <80048862+GAURAV5925@users.noreply.github.com>
2022-07-24 09:45:16 +0530

    Create dictionary2.py

---
+++ Dictionary/dictionary2.py
@@ -0,0 +1,7 @@
+Dict = {1: 'Gaurav', 2: 'Ranjit', 3: 'Jha'}
+print("\nDictionary with the use of Integer Keys:")
+print(Dict)
+
+Dict = {'Name': 'Gaurav', 1: [1,2,3,4]}
+print("\nDictionary with the use of Mixed keys: ")
+print(Dict)
